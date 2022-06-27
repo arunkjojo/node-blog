e64e10292a5c05dc5c05308a41bf120c5a5be38f
Arun K Jojo <arunkjojo@gmail.com>
2022-06-27 16:07:55 +0530

    npm

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const { readFile } = require('fs');
 const http = require('http');
+const _ = require('lodash');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method);
+
+    // lodash
+    const num = _.random(0,20);
+    console.log(num);
+
+    const greet = _.once(()=>{
+        console.log('hello', num)
+    });
+
+    greet();
+    greet();
+
+    // console.log(req.url, req.method);
 
     // set header content type
     res.setHeader('Content-Type', 'text/html');
@@ -17,8 +30,7 @@ const server = http.createServer((req, res) => {
             path += 'about.html';
             res.statusCode = 200;
         break;
-        case '/about-me':
-            case '/about-us':
+        case '/about-us':
             res.statusCode = 301;
             res.setHeader('Location', '/about');
             res.end();
