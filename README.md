6ec735fa3108d0a6f6d959b112cb30f5d75d0b83
Arun K Jojo <arunkjojo@gmail.com>
2022-06-27 17:24:15 +0530

    Node.JS based blig app

--- README.md
+++
@@ -1,2 +0,0 @@
-# node-crash-course
-Course for Node.js applicarion
--- people.js
+++
@@ -1,9 +0,0 @@
-const people = ['aj', 'arun', 'arun jojo', 'jo', 'devaj'];
-const ages = [20,22,23,19,21];
-
-console.log(people);
-
-module.exports = {
-    people,
-    age: ages
-};
